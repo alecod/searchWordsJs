221ddf84fc5ea0dc49f3c81028e1eafab696f466
Ale Junqueira <alessandro.junqueira@intergalaxy.io>
2021-05-31 15:13:45 -0300

    up

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const fn = require('./funcoes')
+
+const caminho = path.join(__dirname, '..', 'dados', 'legendas')
+
+
+const arquivos =  fn.lerDiretorio(caminho)
+
+console.log(arquivos)
