f368258764f2293fd2e7c2be3be9501d203e45c5
Ale Junqueira <alessandro.junqueira@intergalaxy.io>
2021-06-01 11:11:03 -0300

    up

--- src/funcoes.js
+++ src/funcoes.js
@@ -29,17 +29,50 @@ function lerArquivos(caminhos) {
     return Promise.all(caminhos.map(caminho => lerUnicoArquivo(caminho)))
 }
 
-function elementosTerminadosCom(array, padrao) {
-  return array.filter((el) => el.endsWith(padrao));
+function elementosTerminadosCom(padraoTextual) {
+  return function(array) {
+    return array.filter((el) => el.endsWith(padraoTextual));
+  }
+  
 }
 
-function removerSeVazio(array) {
-    return array.filter((el) => el.endsWith)
+function removerElementosSeVazio(array) {
+    return array.filter(el => el.trim())
 } 
+
+function removerElementosSeIncluir(padraoTextual) {
+    return function(array) {
+       return array.filter(el => !el.includes(padraoTextual))
+    }
+    
+}
+
+function removerElementosSeApenasNumero(array) {
+    return array.filter(el => {
+      const numero = parseInt(el.trim())
+      return numero !== numero
+    })
+}
+
+// split serve para remover o elemento e o join unir com um espaco
+function removerSimbolos(simbolos) {
+    return function(array) {
+      return array.map(el => {
+          let novoTexto = el
+          simbolos.forEach(simbolo => {
+             novoTexto = novoTexto.split(simbolo).join('')
+          })
+      })
+    }
+}
+
 
 module.exports = {
   lerDiretorio,
   elementosTerminadosCom,
   lerUnicoArquivo,
   lerArquivos,
+  removerElementosSeVazio,
+  removerElementosSeIncluir,
+  removerElementosSeApenasNumero
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-const { clear } = require("console");
 const path = require("path");
 const fn = require("./funcoes");
 
 const caminho = path.join(__dirname, "..", "dados", "legendas");
 
+const simbolos = [
+    '.', '?', '-', ',', '"', '♪',
+    '_', '<i>', '</i>', '\r', '[', ']',
+    '(', ')'
+]
+
 fn.lerDiretorio(caminho)
-  .then(arquivos => fn.elementosTerminadosCom(arquivos, ".srt"))
-  .then(arquivosSRT => fn.lerArquivos(arquivosSRT))
+  .then(fn.elementosTerminadosCom(".srt"))
+  .then(fn.lerArquivos)
   .then(conteudos => conteudos.join('\n'))
   .then(todoConteudo => todoConteudo.split('\n'))
+  .then(fn.removerElementosSeVazio)
+  .then(fn.removerElementosSeIncluir('-->'))
+  .then(fn.removerElementosSeApenasNumero)
   .then(console.log)
